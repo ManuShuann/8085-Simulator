43cbedf70cb993132fcbe60b6a0101427255cbc4
Manpreet Shuann <manpreetshuann@gmail.com>
2018-04-02 19:49:16 +0530

    1

--- trainig2k16/MAINPROJ.sql
+++
@@ -1,6 +0,0 @@
-create table filenamedata1(id int primary key identity(1,1),name varchar(90),pathname varchar(90))
-select * from filenamedata1
-create table cmd(commands varchar(90))
-delete from filenamedata1 where id=11
-select * from cmd
-delete from cmd where commands='hahhahha'
